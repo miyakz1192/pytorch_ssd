4061ceccc415930c51a79083e33b0826a5acd98b
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2022-12-20 16:14:21 +0000

    gogo.sh updated

--- bin/gogo.sh
+++ bin/gogo.sh
@@ -1,9 +1,19 @@
+
+if [ $# -ne 1 ]; then
+	echo "USAGE: weight file"
+	exit 1
+fi
+
+
 files=`ls imgdata/lu*jpg; ls imgdata/ru*jpg`
+weight=$1
+
+
 
 for line in ${files}
 do
 	echo "file==>" $line
-      	python3 predict.py ./${line}
+      	python3 predict.py ${weight} ./${line}
 	echo "hit enter file==>" $line
 	read -p "Hit enter: ==>"
 done
